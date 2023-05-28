14e6d290ba9c7fad8fd278e7f8c5d1049ce4ede0
Oezguen Turgut <oezguen.turgut@tum.de>
2023-05-28 14:15:21 +0200

    Add: new augmentation SpecAugment()

--- bash_scripts/finetune_ecg.sh
+++ bash_scripts/finetune_ecg.sh
@@ -3,7 +3,7 @@
 
 # Basic parameters seed = [0, 101, 202, 303, 404]
 seed=(0)
-batch_size=(16)
+batch_size=(8)
 accum_iter=(1)
 
 epochs="400"
@@ -39,10 +39,10 @@ layer_decay=(0.75)
 # Optimizer parameters
 blr=(3e-6) # 3e-5 if from scratch
 min_lr="0.0"
-weight_decay=(0.2)
+weight_decay=(0.1)
 
 # Criterion parameters
-smoothing=(0.0)
+smoothing=(0.2)
 
 from_scratch="False"
 
@@ -62,22 +62,22 @@ fi
 # labels_path=$data_base"/ecg/labelsOneHot/labels_train_flutter_all_balanced.pt"
 # downstream_task="classification"
 # nb_classes="2"
-# data_path=$data_base"/ecg/ecgs_train_diabetes_all_balanced_noBase_gn.pt"
-# labels_path=$data_base"/ecg/labelsOneHot/labels_train_diabetes_all_balanced.pt"
-# downstream_task="classification"
-# nb_classes="2"
+data_path=$data_base"/ecg/ecgs_train_diabetes_all_balanced_noBase_gn.pt"
+labels_path=$data_base"/ecg/labelsOneHot/labels_train_diabetes_all_balanced.pt"
+downstream_task="classification"
+nb_classes="2"
 # data_path=$data_base"/ecg/ecgs_train_CAD_all_balanced_noBase_gn.pt"
 # labels_path=$data_base"/ecg/labelsOneHot/labels_train_CAD_all_balanced.pt"
 # downstream_task="classification"
 # nb_classes="2"
-data_path=$data_base"/ecg/ecgs_train_Regression_noBase_gn.pt"
-labels_path=$data_base"/ecg/labelsOneHot/labels_train_Regression_stdNormed.pt"
-labels_mask_path=$data_base"/ecg/labels_train_Regression_mask.pt"
-downstream_task="regression"
-# LV
-lower_bnd="0"
-upper_bnd="6"
-nb_classes="6"
+# data_path=$data_base"/ecg/ecgs_train_Regression_noBase_gn.pt"
+# labels_path=$data_base"/ecg/labelsOneHot/labels_train_Regression_stdNormed.pt"
+# labels_mask_path=$data_base"/ecg/labels_train_Regression_mask.pt"
+# downstream_task="regression"
+# # LV
+# lower_bnd="0"
+# upper_bnd="6"
+# nb_classes="6"
 # # RV
 # lower_bnd="6"
 # upper_bnd="10"
@@ -99,24 +99,24 @@ nb_classes="6"
 # val_data_path=$data_base"/ecg/ecgs_val_ecg_imaging_noBase_gn.pt"
 # val_labels_path=$data_base"/ecg/labelsOneHot/labels_val_flutter_all.pt"
 # pos_label="1"
-# val_data_path=$data_base"/ecg/ecgs_val_ecg_imaging_noBase_gn.pt"
-# val_labels_path=$data_base"/ecg/labelsOneHot/labels_val_diabetes_all.pt"
-# pos_label="1"
+val_data_path=$data_base"/ecg/ecgs_val_ecg_imaging_noBase_gn.pt"
+val_labels_path=$data_base"/ecg/labelsOneHot/labels_val_diabetes_all.pt"
+pos_label="1"
 # val_data_path=$data_base"/ecg/ecgs_val_ecg_imaging_noBase_gn.pt"
 # val_labels_path=$data_base"/ecg/labelsOneHot/labels_val_CAD_all.pt"
 # pos_label="1"
-val_data_path=$data_base"/ecg/ecgs_val_Regression_noBase_gn.pt"
-val_labels_path=$data_base"/ecg/labelsOneHot/labels_val_Regression_stdNormed.pt"
-val_labels_mask_path=$data_base"/ecg/labels_val_Regression_mask.pt"
+# val_data_path=$data_base"/ecg/ecgs_val_Regression_noBase_gn.pt"
+# val_labels_path=$data_base"/ecg/labelsOneHot/labels_val_Regression_stdNormed.pt"
+# val_labels_mask_path=$data_base"/ecg/labels_val_Regression_mask.pt"
 
 global_pool=(True)
 attention_pool=(True)
-num_workers="24"
+num_workers="32"
 
 # Log specifications
-save_output="True"
+save_output="False"
 wandb="True"
-wandb_project="MAE_ECG_Fin_Tiny_LV"
+wandb_project="MAE_ECG_Fin_Tiny_Diabetes_v1"
 wandb_id=""
 
 # Pretraining specifications
@@ -146,20 +146,21 @@ do
                         for smth in "${smoothing[@]}"
                         do
 
-                            folder="ecg/LV/MM"
-                            subfolder=("seed$sd/"$model_size"/t2500/p"$patch_height"x"$patch_width"/ld"$ld"/dp"$dp"/smth"$smth"/wd"$weight_decay"/m0.8/atp")
+                            folder="ecg/Diabetes"
+                            subfolder=("seed$sd/"$model_size"/t2500/p"$patch_height"x"$patch_width"/ld"$ld"/dp"$dp"/smth"$smth"/wd"$weight_decay"/m0.8")
 
                             pre_data="b"$pre_batch_size"_blr"$pre_blr
                             # finetune=$checkpoint_base"/sprai/mae_he/mae/output/pre/"$folder"/"$subfolder"/pre_"$pre_data"/checkpoint-399.pth"
                             # finetune=$checkpoint_base"/ECGMultimodalContrastiveLearning/oezguen/checkpoints/mm_v230_mae_checkpoint.pth"
-                            finetune=$checkpoint_base"/ECGMultimodalContrastiveLearning/oezguen/checkpoints/mm_v283_mae_checkpoint.pth"
+                            # finetune=$checkpoint_base"/ECGMultimodalContrastiveLearning/oezguen/checkpoints/mm_v283_mae_checkpoint.pth"
                             # finetune=$checkpoint_base"/ECGMultimodalContrastiveLearning/pretrained_checkpoints/tiny/v1/checkpoint-399.pth"
-                            # finetune=$checkpoint_base"/sprai/mae_he/mae/output/pre/ecg/seed0/tiny/t2500/p1x100/wd0.15/m0.8/pre_b128_blr1e-5/checkpoint-383-ncc-0.95.pth"
+                            # finetune=$checkpoint_base"/sprai/mae_he/mae/output/pre/ecg/ncc_weight0.3/seed0/tiny/t2500/p1x100/wd0.15/m0.8/pre_b128_blr1e-5/checkpoint-399-ncc-0.96.pth"
+                            finetune=$checkpoint_base"/ECGMultimodalContrastiveLearning/oezguen/checkpoints/ecg/ecg_v100_mae_checkpoint.pth"
 
                             output_dir=$checkpoint_base"/sprai/mae_he/mae/output/fin/"$folder"/"$subfolder"/fin_b"$(($bs*$accum_iter))"_blr"$lr"_"$pre_data
                             log_dir=$checkpoint_base"/sprai/mae_he/mae/logs/fin/"$folder"/"$subfolder"/fin_b"$(($bs*$accum_iter))"_blr"$lr"_"$pre_data
 
-                            resume=$checkpoint_base"/sprai/mae_he/mae/output/fin/"$folder"/"$subfolder"/fin_b"$bs"_blr"$lr"_"$pre_data"/checkpoint-4-pcc-0.54.pth"
+                            # resume=$checkpoint_base"/sprai/mae_he/mae/output/fin/"$folder"/"$subfolder"/fin_b"$bs"_blr"$lr"_"$pre_data"/checkpoint-4-pcc-0.54.pth"
 
                             if [ "$downstream_task" = "regression" ]; then
                                 cmd="python3 main_finetune.py --lower_bnd $lower_bnd --upper_bnd $upper_bnd --seed $sd --downstream_task $downstream_task --jitter_sigma $jitter_sigma --rescaling_sigma $rescaling_sigma --ft_surr_phase_noise $ft_surr_phase_noise --input_channels $input_channels --input_electrodes $input_electrodes --time_steps $time_steps --patch_height $patch_height --patch_width $patch_width --model $model --batch_size $bs --epochs $epochs --patience $patience --max_delta $max_delta --accum_iter $accum_iter --drop_path $dp --weight_decay $wd --layer_decay $ld --min_lr $min_lr --blr $lr --warmup_epoch $warmup_epochs --smoothing $smth --data_path $data_path --labels_path $labels_path --val_data_path $val_data_path --val_labels_path $val_labels_path --nb_classes $nb_classes --log_dir $log_dir --num_workers $num_workers"
